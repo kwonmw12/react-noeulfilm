b83a1dc3e248835a86a7d1b3e0fbed73540b997c
kwonmw12 <kwonmw12@gmail.com>
2022-12-28 18:12:29 +0900

    서브스와이퍼 튜닝중

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import "swiper/css/grid";
+
 
 import Header from './component/Hd';
 import Worksswiper from './component/Swiper';
@@ -13,8 +15,8 @@ function App() {
   return (
     <div className="App" id="top">
       <Header></Header>
-      <Worksswiper contentid="mainswiper"></Worksswiper>  
-      <Worksswiper contentid="subswiper" title="BEST PHOTO" subtitle="노을필름에서 가장 인기있는 사진을 소개합니다."></Worksswiper>    
+      <Worksswiper contentid="mainswiper" delay="3000" view="1.5" ></Worksswiper>  
+      <Worksswiper contentid="subswiper" delay="9000000" view="6" rows="1"   title="BEST PHOTO" subtitle="노을필름에서 가장 인기있는 사진을 소개합니다."></Worksswiper>    
       <Preinterview contentid="me" objnm="noticeObj" title="아닐껄아마도?"></Preinterview>
       <Preinterview contentid="preInterview" objnm="preinterview" title="사전인터뷰"></Preinterview>
       <Footer></Footer>
--- src/component/Swiper.js
+++ src/component/Swiper.js
@@ -1,35 +1,36 @@
 import swiperDBlink from '../json/swiperinof.json';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, Grid } from 'swiper';
 
 
 const Worksswiper = (props) => {
     const swiperinfo = swiperDBlink[props.contentid];
       return(
         <section className="section text-center" id={props.contentid}>
-          <h3>{props.title}</h3>
-          <p>{props.subtitle}</p>
+          <h3 className='swiper_h3'>{props.title}</h3>
+          <p className='swiper_p'>{props.subtitle}</p>
           <Swiper className='workSection'
-          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, Grid]}
           breakpoints={{
             768:{
               slidesPerView:1             
               },
             1024:{
-              slidesPerView:1.5
+              slidesPerView:props.view
               }
           }}
+          grid={{rows:props.rows}}
           spaceBetween={0}
           centeredSlides={true}
+          
           autoplay={{
-          delay: 1000000000,
+          delay:props.delay,
           disableOnInteraction: false,
           }}
           loop = {true} 
           pagination={{ clickable: true }}
           scrollbar={{ draggable: true }}
           onSlideChange={() => {
-          //슬라이드가 끝나고 실행
           }}
           onSwiper={(swiper) => console.log(swiper)}>
           
